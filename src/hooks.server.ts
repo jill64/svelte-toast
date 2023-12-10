6d0dab53b00ef92958f619bfc01f9a16730a9f53
jill64 <intents.turrets0h@icloud.com>
2023-12-11 04:33:07 +0900

    chore: refactor demo with npm-demo-layout

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,9 +1,9 @@
 import { serverInit } from '@jill64/sentry-sveltekit-cloudflare'
-import { onRender } from '@jill64/svelte-dark-theme'
+import { hooks } from '@jill64/npm-demo-layout'
 
 const { onHandle, onError } = serverInit(
   'https://a002be94e5a9dfad1fdc56df5ea34f4b@o4505814639312896.ingest.sentry.io/4506253567328256'
 )
 
-export const handle = onHandle(onRender())
+export const handle = onHandle(hooks)
 export const handleError = onError()
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,46 +1,8 @@
 <script lang="ts">
-  import Toaster from '$lib/Toaster.svelte'
-  import { FlipButton, ThemeManager, theme } from '@jill64/svelte-dark-theme'
-  import github from 'svelte-highlight/styles/github'
-  import githubDark from 'svelte-highlight/styles/github-dark'
-  import GItHubLogo from './GItHubLogo.svelte'
+  import { Layout } from '@jill64/npm-demo-layout'
+  import packageJson from '../../package.json'
 </script>
 
-<svelte:head>
-  <!-- eslint-disable-next-line svelte/no-at-html-tags -->
-  {@html $theme === 'dark' ? githubDark : github}
-</svelte:head>
-
-<Toaster dark={$theme === 'dark'} />
-<ThemeManager />
-
-<header>
-  <hgroup>
-    <h1>@jill64/svelte-toast</h1>
-    <p>🍞 Pre-Themed Responsive Toast Notification</p>
-  </hgroup>
-  <FlipButton />
-  <a href="https://github.com/jill64/svelte-toast">
-    <GItHubLogo />
-  </a>
-</header>
-<slot />
-
-<style>
-  :global(body) {
-    font-family: sans-serif;
-    margin: 1rem;
-  }
-  :global(.dark body) {
-    background: #161616;
-    color: whitesmoke;
-  }
-  header {
-    display: flex;
-    align-items: center;
-    gap: 1rem;
-  }
-  hgroup {
-    margin-right: auto;
-  }
-</style>
+<Layout {packageJson}>
+  <slot />
+</Layout>
