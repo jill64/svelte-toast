81ba7c4c263e09a22d829f95bdf065c69e4b05ef
jill64 <intents.turrets0h@icloud.com>
2024-02-05 11:28:25 +0900

    chore: fix sentry dsn

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,7 +2,7 @@ import { hooks } from '@jill64/npm-demo-layout'
 import { init } from '@jill64/sentry-sveltekit-cloudflare/server'
 
 const { onHandle, onError } = init(
-  'https://a002be94e5a9dfad1fdc56df5ea34f4b@o4505814639312896.ingest.sentry.io/4506253567328256'
+  'https://4f794b1c54f570c819ef0486a7c7ae40@o4505814639312896.ingest.sentry.io/4506691816980480'
 )
 
 export const handle = onHandle(hooks)
